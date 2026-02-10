d98a75bec1371aa696f48cd644476c5ba33971e6
Heorhii Churhuliia <gio4929189@gmail.com>
2026-02-10 12:18:23 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,20 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import card from "../models/card.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-    response.send('About the Playlist app');   
+
+    const viewData = {
+      title: "About the Playlist app",
+      info: card.getAppInfo()
+    };
+
+    //logger.debug(viewData);
+    response.render('about', viewData);   
+    //
   },
 };
 
--- server.js
+++ server.js
@@ -3,9 +3,19 @@
 import express from 'express';
 import routes from './routes.js';
 import logger from './utils/logger.js';
+import { create } from 'express-handlebars';
+
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+
+const handlers = create({extname: '.hbs'});
+app.engine('.hbs', handlers.engine);
+app.set('view engine', '.hbs');
+
+
 
 app.use('/', routes);
 
