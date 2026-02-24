fb53ffcf8451371fd0b38cef36897383414ee7df
Heorhii Churhuliia <gio4929189@gmail.com>
2026-02-24 12:05:42 +0000

    Completed Dynamic Views and Helpers Lab

--- controllers/about.js
+++ controllers/about.js
@@ -1,22 +1,18 @@
 'use strict';
-
 import logger from "../utils/logger.js";
-import card from "../models/card.js";
+import empStore from "../models/emp-store.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-
-    const viewData = {
-      title: "About the Playlist app",
-      info: card.getAppInfo()
+   
+      const viewData = {
+      title: "Playlist App About",
+      emps: empStore.getEmpInfo()
     };
-
-    //logger.debug(viewData);
-    response.render('about', viewData);   
-    //
+    logger.info(viewData.emps)
+    response.render('about', viewData); 
   },
 };
-
 
 export default about;
